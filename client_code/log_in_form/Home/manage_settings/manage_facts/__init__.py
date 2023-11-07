e2630c1f2a9743f3512933625b651acf8968a1c8
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-07 11:31:24 +0000

    Edited log_in_form.Home.manage_settings.manage_facts

--- client_code/log_in_form/Home/manage_settings/manage_facts/__init__.py
+++ client_code/log_in_form/Home/manage_settings/manage_facts/__init__.py
@@ -5,6 +5,14 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+
+def add_data_to_database(processing_fee, extension_fee, default_fee):
+    # Replace 'your_table_name' with the actual name of your database table
+    your_table = app_tables.manage_settings
+    # Add a new row to the database table
+    new_row = your_table.add_row(processing_fee=processing_fee, extension_fee=extension_fee, default_fee=default_fee)
+
+
 class manage_facts(manage_factsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
