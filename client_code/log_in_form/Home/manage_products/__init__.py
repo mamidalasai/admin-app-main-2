ce5f0928c12502cd20da84517e13dd3026b6474d
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-08 12:26:15 +0000

    Edited log_in_form.Home.manage_products

--- client_code/log_in_form/Home/manage_products/__init__.py
+++ client_code/log_in_form/Home/manage_products/__init__.py
@@ -27,7 +27,49 @@ class manage_products(manage_productsTemplate):
 
   def text_box_4_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    product_id = self.text_box_1.text
+    product_name = self.text_box_2.text
+    membership_type = self.drop_down_4.selected_value
+    processing_fee = self.text_box_4.text
+    extension_fee = self.text_box_5.text
+    defaulter_fee = self.text_box_6.text
+    interest_type = self.drop_down_1.selected_value
+    max_amount = self.text_box_7.text
+    min_amount = self.text_box_8.text
+    tenure = self.text_box_9.text
+    extensions_allowed = self.drop_down_2.selected_value
+    part_payments = self.drop_down_3.selected_value
+    fore_closure = self.text_box_10.text
+
+    if product_id == "" and product_name == '' and membership_type == '' and processing_fee == '' and extension_fee == '' and defaulter_fee == '' and interest_type == '' and max_amount == '' and min_amount == '' and tenure == '' and extensions_allowed == '' and part_payments == '' and fore_closure == '':
+      Notification("Enter All Feilds").show()
+    elif product_id == "":
+      Notification('Enter Your Product_id').show()
+    elif product_name == "":
+      Notification("Enter Your Product_name").show()
+    elif product_name == "":
+      Notification("Enter Your Product_name").show()
+      elif product_name == "":
+      Notification("Enter Your Product_name").show()
+    elif product_name == "":
+      Notification("Enter Your Product_name").show()
+  elif product_name == "":
+      Notification("Enter Your Product_name").show()
+elif product_name == "":
+      Notification("Enter Your Product_name").show()
+elif product_name == "":
+      Notification("Enter Your Product_name").show()
+elif product_name == "":
+      Notification("Enter Your Product_name").show()
+elif product_name == "":
+      Notification("Enter Your Product_name").show()
+elif product_name == "":
+      Notification("Enter Your Product_name").show()elif product_name == "":
+      Notification("Enter Your Product_name").show()elif product_name == "":
+      Notification("Enter Your Product_name").show()
     
     
     
