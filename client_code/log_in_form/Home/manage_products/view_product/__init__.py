f9d478ea9e9f76b1aca837bf9e7ff8ddb299dfb6
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-22 14:44:07 +0000

    Edited log_in_form.Home.manage_products.view_product, log_in_form.Home.manage_products.edit_form, log_in_form.Home.manage_products.manage_products1 and log_in_form.Home.manage_products.update_form

--- client_code/log_in_form/Home/manage_products/view_product/__init__.py
+++ client_code/log_in_form/Home/manage_products/view_product/__init__.py
@@ -12,7 +12,7 @@ class view_product(view_productTemplate):
 
     # Any code you write here will run before the form opens.
 
-        self.data = tables.app_tables.user_profile.search()
+    self.data = tables.app_tables.product_details.search()
 
     a = -1
     self.list_1 = []
@@ -25,9 +25,9 @@ class view_product(view_productTemplate):
       a+=1
       self.list_1.append(i['product_id'])
       self.list_2.append(i['product_name'])
-      self.list_3.append(i[''])
-      self.list_4.append(i['gender'])
-      self.list_5.append(i['user_age'])
+      self.list_3.append(i['product_categories'])
+      self.list_4.append(i['processing_fee'])
+      self.list_5.append(i['extension_fee'])
 
     print(a)
 
@@ -37,8 +37,7 @@ class view_product(view_productTemplate):
     else:
       for i in range(a+1):
         print(self.list_2[i])
-        self.result.append({'coustmer_id' : self.list_1[i], 'full_name' : self.list_2[i], 'profile_status' : self.list_3[i], 'gender' : self.list_4[i], 'user_age' : self.list_5[i],
-                          'date_of_birth' : self.list_6[i], 'mobile' : self.list_7[i], 'aadhaar_no' : self.list_8[i]})
+        self.result.append({'product_id' : self.list_1[i], 'product_name' : self.list_2[i], 'product_categories' : self.list_3[i], 'processing_fee' : self.list_4[i], 'extension_fee' : self.list_5[i]})
 
       self.repeating_panel_1.items = self.result
 
