b2c9c708260617003729f6c3cf967d6b6dcead39
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-23 15:19:13 +0000

    Created log_in_form.Home.manage_products.add_categories; Edited log_in_form.Home.manage_products.edit_form, log_in_form.Home.manage_products and log_in_form.Home.manage_products.manage_products1; Deleted log_in_form.Home.manage_products.view_categories

--- client_code/log_in_form/Home/manage_products/__init__.py
+++ client_code/log_in_form/Home/manage_products/__init__.py
@@ -23,6 +23,10 @@ class manage_products(manage_productsTemplate):
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
     open_form('log_in_form.Home')
+
+  def link_4_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('log_in_form.Home.manage_products.add_categories')
     
     
     
