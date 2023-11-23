bd2ab6c7b69526a072365651a971ab4de6a93550
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-23 09:32:38 +0000

    Created log_in_form.Home.manage_products.view_product.view_profile

---
+++ client_code/log_in_form/Home/manage_products/view_product/view_profile/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import view_profileTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class view_profile(view_profileTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
