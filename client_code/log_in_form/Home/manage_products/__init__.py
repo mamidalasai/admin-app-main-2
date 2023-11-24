51b538c44fa9018775aea8d8e3e3266cf0c5b017
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-24 17:50:33 +0000

    Edited log_in_form.Home.manage_products and log_in_form.Home.manage_products.manage_products1

--- client_code/log_in_form/Home/manage_products/__init__.py
+++ client_code/log_in_form/Home/manage_products/__init__.py
@@ -6,7 +6,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class manage_products(manage_productsTemplate):
-  def __init__(self, **properties):
+  def __init__(self,product_id, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
@@ -26,11 +26,11 @@ class manage_products(manage_productsTemplate):
 
   def link_4_click(self, **event_args):
     """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_products.add_categories')
+    open_form('log_in_form.Home.manage_products.add_categories',get_customer_id_value,product_id)
 
   def link_5_click(self, **event_args):
     """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_products.add_groups')
+    open_form('log_in_form.Home.manage_products.add_groups',get_customer_id_value,product_id)
     
     
     
