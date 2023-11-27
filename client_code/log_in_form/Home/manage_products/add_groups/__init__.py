594d0c0942c78bd6c70a9c626118e70501dd3178
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-27 08:15:43 +0000

    Edited ServerModule1, log_in_form.Home.manage_products, log_in_form.Home.manage_products.manage_products1 and log_in_form.Home.manage_products.add_groups

--- client_code/log_in_form/Home/manage_products/add_groups/__init__.py
+++ client_code/log_in_form/Home/manage_products/add_groups/__init__.py
@@ -18,7 +18,7 @@ class add_groups(add_groupsTemplate):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    product_group = self.text_box_1.text
-    open_form('log_in_form.Home.manage_products.manage_products1',product_group)
+    text_data = self.text_box_1.text
+    open_form('log_in_form.Home.manage_products')
 
 
