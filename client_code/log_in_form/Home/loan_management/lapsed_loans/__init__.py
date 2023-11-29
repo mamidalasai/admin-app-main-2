f15b5e276f6dd75e0b0aa824c7d64f8d67f7ddd9
saimamidala333@gmail.com <saimamidala333@gmail.com>
2023-11-29 15:02:56 +0000

    Created log_in_form.Home.loan_management.lapsed_loans

---
+++ client_code/log_in_form/Home/loan_management/lapsed_loans/__init__.py
@@ -0,0 +1,89 @@
+from ._anvil_designer import lapsed_loansTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+from datetime import datetime
+
+class lapsed_loans(lapsed_loansTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+    self.loan = tables.app_tables.loan_details.search()
+    self.fourcloser = tables.app_tables.foreclosure.search()
+    self.today = datetime.now().date()
+    print(self.today)
+
+    self.due_list = []
+    self.loan_amont = []
+    self.paid_amount = []
+    self.intrest = []
+    self.loan_due_amount = []
+
+    self.id = []
+    self.c_id = []
+    self.full_name = []
+    for i in self.loan:
+      self.due_list.append(i['due_date'].date())
+      self.loan_amont.append(i['loan_amount'])
+      self.id.append(i['loan_id'])
+      self.intrest.append(i['interest_rate'])
+      self.loan_due_amount.append(i['total_repayment_amount'])
+      self.c_id.append(i['coustmer_id'])
+      self.full_name.append(i['full_name'])
+
+    self.id_1 = []
+    for i in self.fourcloser:
+      self.paid_amount.append(i['paid_amount'])
+      self.id_1.append(i['loan_id'])
+
+
+    self.index = []  
+    for i in self.id:
+      if i in self.id_1:
+        b = self.id_1.index(i)
+        if self.loan_amont[b] != self.paid_amount[b]:
+          self.index.append(self.id_1[b])
+
+    self.result = []
+    self.days = {}
+    for i in self.index:
+      c = self.index.index(i)
+      if self.due_list[c] < self.today:
+        annual_interest_rate = self.intrest[c]
+        days_in_year = 365
+        daily_interest_rate = (annual_interest_rate / 100) / days_in_year
+        print(daily_interest_rate)
+        self.result.append(self.id[c])
+        interest_per_day = self.loan_due_amount[c] * daily_interest_rate
+        days_late = self.today.day - self.due_list[c].day
+        penalty = interest_per_day * days_late
+        total_due = self.loan_due_amount[c] + penalty
+        self.days[self.id[c]] = total_due
+    
+    print(self.result)
+    print(self.days)
+    self.index1 = []
+    self.final = []
+    self.total = []
+    for id, total in self.days.items():
+      self.index1.append(self.id.index(id))
+      self.total.append(total)
+
+    a = -1
+    for i in self.index1:
+      a += 1
+      self.final.append({'loan_id' : self.id[i], 'coustmer_id' : self.c_id[i], 'full_name' : self.full_name[i], 'amount': int(self.total[a])})
+
+    self.repeating_panel_1.items = self.final
+    print(self.index1)
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('admin.dashboard.loan_management')
